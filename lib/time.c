d4c18cf76488004c55af633ec87ac0c12550d399
Jerome Forissier <jerome.forissier@linaro.org>
2025-01-13 18:06:21 +0100

    efi_loader: optimize efi_init_obj_list() with coroutines
    When COROUTINES is enabled, schedule efi_disks_register() and
    efi_tcg2_register() as two coroutines in efi_init_obj_list() instead of
    invoking them sequentially. The voluntary yield point is introduced
    inside udelay() which is called frequently as a result of each function
    polling the hardware. This allows the two coroutines to make progress
    simultaneously and reduce the wall clock time required by
    efi_init_obj_list().
    
    Tested on Kria KV260 with a microSD card inserted with the "printenv
    -e" command. With COROUTINES disabled, efi_init_obj_list() completes in
    2821 ms on average (2825, 2822, 2817). With COROUTINES enabled, it
    takes 2265 ms (2262, 2260, 2272). That is a reduction of 556 ms which
    is not bad at all considering that measured separately,
    efi_tcg2_register() takes ~825 ms and efi_disks_register() needs ~600
    ms, so assuming they would overlap perfectly one can expect a 600 ms
    improvement at best.
    
    The code size penalty for this improvement is 1340 bytes.
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

--- lib/time.c
+++ lib/time.c
@@ -17,6 +17,7 @@
 #include <asm/global_data.h>
 #include <asm/io.h>
 #include <linux/delay.h>
+#include <coroutines.h>
 
 #ifndef CFG_WD_PERIOD
 # define CFG_WD_PERIOD	(10 * 1000 * 1000)	/* 10 seconds default */
@@ -190,6 +191,8 @@ void __weak __udelay(unsigned long usec)
 
 /* ------------------------------------------------------------------------- */
 
+int udelay_yield;
+
 void udelay(unsigned long usec)
 {
 	ulong kv;
@@ -197,7 +200,16 @@ void udelay(unsigned long usec)
 	do {
 		schedule();
 		kv = usec > CFG_WD_PERIOD ? CFG_WD_PERIOD : usec;
-		__udelay(kv);
+		if (CONFIG_IS_ENABLED(COROUTINES) &&
+		    udelay_yield == 0xCAFEDECA) {
+			ulong t0 = timer_get_us();
+			do {
+				co_yield();
+				__udelay(10);
+			} while (timer_get_us() < t0 + kv);
+		} else {
+			__udelay(kv);
+		}
 		usec -= kv;
 	} while(usec);
 }
