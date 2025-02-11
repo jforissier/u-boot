293fa247f89d9d5f0deccb6bf4350e4e8031d3d5
Jerome Forissier <jerome.forissier@linaro.org>
2025-02-11 15:58:22 +0100

    arch: introduce symbol HAVE_INITJMP
    HAVE_INIJMP will be set by architectures that support initjmp(), a
    non-standard extension to setjmp()/longjmp() allowing to initialize a
    jump buffer with a function pointer and a stack pointer. This will be
    useful to later introduce threads. With this new function it becomes
    possible to longjmp() to a particular function pointer (rather than
    to a point previously reached during program execution as is the case
    with setjmp()), and with a custom stack. Both things are needed to
    spin off a new thread. Then the usual setjmp()/ longjmp() pair is
    enough to save and restore a context, i.e., switch thread.
    The initjmp() prototype is added to <asm-generic/setjmp.h> since it
    is common to all architectures.
    
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>
    Reviewed-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

--- include/asm-generic/setjmp.h
+++ include/asm-generic/setjmp.h
@@ -17,4 +17,25 @@ typedef struct jmp_buf_data jmp_buf[1];
 int setjmp(jmp_buf jmp);
 __noreturn void longjmp(jmp_buf jmp, int ret);
 
+#ifdef CONFIG_HAVE_INITJMP
+/**
+ * initjmp - initialize jmp_buf to branch to a given function with a given stack
+ *
+ * This function sets up a jump buffer for later use with longjmp(). In the
+ * traditional setjmp()/longjmp() pair, longjmp() branches immediately after the
+ * setjmp() which appears to return a non-zero status to indicate that the long
+ * jump has occured. The stack is restored as it was saved in the jump buffer by
+ * setjmp(). This initjmp() function however allows to use longjmp() to branch
+ * to any function and with a specific stack. Please note that @func MUST NOT
+ * return. It shall typically restore the main stack and resume execution by
+ * simply doing a longjmp() to a jump buffer set in the main thread before the
+ * longjmp() call. initjmp() allows to implement multithreading.
+ *
+ * @jmp: jump buffer to initialize
+ * @func: function to be called on longjm(), MUST NOT RETURN
+ * @stack_top: the stack to be used by @func
+ */
+int initjmp(jmp_buf jmp, void __noreturn (*func)(void), void *stack_top);
+#endif
+
 #endif /* _SETJMP_H_ */
