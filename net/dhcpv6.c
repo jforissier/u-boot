jforissier/u-boot

commit a0245818f7f8e375abc00f36ff88326331e4e2f9

net: dhcp6: Add DHCPv6 (DHCP for IPv6)
Adds DHCPv6 protocol to u-boot.

Allows for address assignement with DHCPv6 4-message exchange
(SOLICIT->ADVERTISE->REQUEST->REPLY).  Includes DHCPv6 options
required by RFC 8415.  Also adds DHCPv6 options required
for PXE boot.

Possible enhancements:
- Duplicate address detection on DHCPv6 assigned address
- IPv6 address assignement through SLAAC
- Sending/parsing other DHCPv6 options (NTP, DNS, etc...)

Signed-off-by: Sean Edmond <seanedmond@microsoft.com>
Reviewed-by: Ramon Fried <rfried.dev@gmail.com>

net/dhcpv6.h
@@ -1,0 +1,256 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) Microsoft Corporation
+ * Author: Sean Edmond <seanedmond@microsoft.com>
+ *
+ */
+
+#ifndef __DHCP6_H__
+#define __DHCP6_H__
+
+/* Message types */
+#define DHCP6_MSG_SOLICIT	1
+#define DHCP6_MSG_ADVERTISE	2
+#define DHCP6_MSG_REQUEST	3
+#define DHCP6_MSG_REPLY		7
+
+/* Option Codes */
+#define DHCP6_OPTION_CLIENTID		1
+#define DHCP6_OPTION_SERVERID		2
+#define DHCP6_OPTION_IA_NA		3
+#define DHCP6_OPTION_IA_TA		4
+#define DHCP6_OPTION_IAADDR		5
+#define DHCP6_OPTION_ORO		6
+#define DHCP6_OPTION_PREFERENCE		7
+#define DHCP6_OPTION_ELAPSED_TIME	8
+#define DHCP6_OPTION_STATUS_CODE	13
+#define DHCP6_OPTION_OPT_BOOTFILE_URL	59
+#define DHCP6_OPTION_OPT_BOOTFILE_PARAM	60
+#define DHCP6_OPTION_SOL_MAX_RT		82
+#define DHCP6_OPTION_CLIENT_ARCH_TYPE	61
+#define DHCP6_OPTION_VENDOR_CLASS	16
+#define DHCP6_OPTION_NII		62
+
+/* DUID */
+#define DUID_TYPE_LL		3
+#define DUID_HW_TYPE_ENET	1
+#define DUID_LL_SIZE		(sizeof(struct dhcp6_option_duid_ll) + ETH_ALEN)
+#define DUID_MAX_SIZE		DUID_LL_SIZE /* only supports DUID-LL currently */
+
+/* vendor-class-data to send in vendor clas option */
+#define DHCP6_VCI_STRING	"U-boot"
+
+#define DHCP6_MULTICAST_ADDR	"ff02::1:2"	/* DHCP multicast address */
+
+/* DHCP6 States supported */
+enum dhcp6_state {
+	DHCP6_INIT,
+	DHCP6_SOLICIT,
+	DHCP6_REQUEST,
+	DHCP6_DONE,
+	DHCP6_FAIL,
+};
+
+/* DHCP6 Status codes */
+enum dhcp6_status {
+	DHCP6_SUCCESS = 0,
+	DHCP6_UNSPEC_FAIL = 1,
+	DHCP6_NO_ADDRS_AVAIL = 2,
+	DHCP6_NO_BINDING = 3,
+	DHCP6_NOT_ON_LINK = 4,
+	DHCP6_USE_MULTICAST = 5,
+	DHCP6_NO_PREFIX_AVAIL = 6,
+};
+
+/* DHCP6 message header format */
+struct dhcp6_hdr {
+	unsigned int msg_type : 8;	/* message type */
+	unsigned int trans_id : 24;	/* transaction ID */
+} __packed;
+
+/* DHCP6 option header format */
+struct dhcp6_option_hdr {
+	__be16	option_id;	/* option id */
+	__be16	option_len;	/* Option length */
+	u8	option_data[0];	/* Option data */
+} __packed;
+
+/* DHCP6_OPTION_CLIENTID option (DUID-LL) */
+struct dhcp6_option_duid_ll {
+	__be16	duid_type;
+	__be16	hw_type;
+	u8	ll_addr[0];
+} __packed;
+
+/* DHCP6_OPTION_ELAPSED_TIME option */
+struct dhcp6_option_elapsed_time {
+	__be16	elapsed_time;
+} __packed;
+
+/* DHCP6_OPTION_IA_TA option */
+struct dhcp6_option_ia_ta {
+	__be32	iaid;
+	u8	ia_ta_options[0];
+} __packed;
+
+/* DHCP6_OPTION_IA_NA option */
+struct dhcp6_option_ia_na {
+	__be32	iaid;
+	__be32	t1;
+	__be32	t2;
+	u8	ia_na_options[0];
+} __packed;
+
+/* OPTION_ORO option */
+struct dhcp6_option_oro  {
+	__be16	req_option_code[0];
+} __packed;
+
+/* DHCP6_OPTION_CLIENT_ARCH_TYPE option */
+struct dhcp6_option_client_arch {
+	__be16	arch_type[0];
+} __packed;
+
+/* vendor-class-data inside OPTION_VENDOR_CLASS option */
+struct vendor_class_data {
+	__be16	vendor_class_len;
+	u8	opaque_data[0];
+} __packed;
+
+/* DHCP6_OPTION_VENDOR_CLASS option */
+struct dhcp6_option_vendor_class {
+	__be32				enterprise_number;
+	struct vendor_class_data	vendor_class_data[0];
+} __packed;
+
+/**
+ * struct dhcp6_rx_pkt_status - Structure that holds status
+ *                              from a received message
+ * @client_id_match: Client ID was found and matches DUID sent
+ * @server_id_found: Server ID was found in the message
+ * @server_uid_ptr: Pointer to received server ID
+ * @server_uid_size: Size of received server ID
+ * @ia_addr_found: IA addr option was found in received message
+ * @ia_addr_ipv6: The IPv6 address received in IA
+ * @ia_status_code: Status code received in the IA
+ * @status_code: Top-level status code received
+ * @preference: Preference code received
+ */
+struct dhcp6_rx_pkt_status {
+	bool			client_id_match;
+	bool			server_id_found;
+	uchar			*server_uid_ptr;
+	u16			server_uid_size;
+	bool			ia_addr_found;
+	struct in6_addr		ia_addr_ipv6;
+	enum dhcp6_status	ia_status_code;
+	enum dhcp6_status	status_code;
+	u8			preference;
+};
+
+/**
+ * struct dhcp6_server_uid - Structure that holds the server UID
+ *                             received from an ADVERTISE and saved
+ *                             given the server selection criteria.
+ * @uid_ptr: Dynamically allocated and copied server UID
+ * @uid_size: Size of the server UID in uid_ptr (in bytes)
+ * @preference: Preference code associated with this server UID
+ */
+struct dhcp6_server_uid {
+	uchar	*uid_ptr;
+	u16	uid_size;
+	u8	preference;
+};
+
+/**
+ * struct dhcp6_sm_params - Structure that holds DHCP6
+ *                          state machine parameters
+ * @curr_state: current DHCP6 state
+ * @next_state: next DHCP6 state
+ * @dhcp6_start_ms: timestamp DHCP6 start
+ * @dhcp6_retry_start_ms: timestamp of current TX message start
+ * @dhcp6_retry_ms: timestamp of last retransmission
+ * @retry_cnt: retry count
+ * @trans_id: transaction ID
+ * @ia_id: transmitted IA ID
+ * @irt_ms: Initial retransmission time (in ms)
+ * @mrt_ms: Maximum retransmission time (in ms)
+ * @mrc: Maximum retransmission count
+ * @mrd_ms: Maximum retransmission duration (in ms)
+ * @rt_ms: retransmission timeout (is ms)
+ * @rt_prev_ms: previous retransmission timeout
+ * @rx_status: Status from received message
+ * @server_uid: Saved Server UID for selected server
+ * @duid: pointer to transmitted Client DUID
+ */
+struct dhcp6_sm_params {
+	enum dhcp6_state		curr_state;
+	enum dhcp6_state		next_state;
+	ulong				dhcp6_start_ms;
+	ulong				dhcp6_retry_start_ms;
+	ulong				dhcp6_retry_ms;
+	u32				retry_cnt;
+	u32				trans_id;
+	u32				ia_id;
+	int				irt_ms;
+	int				mrt_ms;
+	int				mrc;
+	int				mrd_ms;
+	int				rt_ms;
+	int				rt_prev_ms;
+	struct dhcp6_rx_pkt_status	rx_status;
+	struct dhcp6_server_uid		server_uid;
+	char				duid[DUID_MAX_SIZE];
+};
+
+/* Starts a DHCPv6 4-message exchange as a DHCPv6 client. On successful exchange,
+ * the DHCPv6 state machine will transition from internal states:
+ *	DHCP6_INIT->DHCP6_SOLICIT->DHCP6_REQUEST->DHCP6_DONE
+ *
+ * Transmitted SOLICIT and REQUEST packets will set/request the minimum required
+ * DHCPv6 options to PXE boot.
+ *
+ * After a successful exchange, the DHCPv6 assigned address will be set in net_ip6
+ *
+ * Additionally, the following will be set after receiving these options:
+ * DHCP6_OPTION_OPT_BOOTFILE_URL (option 59) -> net_server_ip6, net_boot_file_name
+ * DHCP6_OPTION_OPT_BOOTFILE_PARAM (option 60) - > pxelinux_configfile
+ *
+ * Illustration of a 4-message exchange with 2 servers (copied from
+ * https://www.rfc-editor.org/rfc/rfc8415):
+ *
+ *               Server                          Server
+ *           (not selected)      Client        (selected)
+ *
+ *                 v               v               v
+ *                 |               |               |
+ *                 |     Begins initialization     |
+ *                 |               |               |
+ *    start of     | _____________/|\_____________ |
+ *    4-message    |/ Solicit      | Solicit      \|
+ *    exchange     |               |               |
+ *             Determines          |          Determines
+ *            configuration        |         configuration
+ *                 |               |               |
+ *                 |\              |  ____________/|
+ *                 | \________     | /Advertise    |
+ *                 | Advertise\    |/              |
+ *                 |           \   |               |
+ *                 |      Collects Advertises      |
+ *                 |             \ |               |
+ *                 |     Selects configuration     |
+ *                 |               |               |
+ *                 | _____________/|\_____________ |
+ *                 |/ Request      |  Request     \|
+ *                 |               |               |
+ *                 |               |     Commits configuration
+ *                 |               |               |
+ *    end of       |               | _____________/|
+ *    4-message    |               |/ Reply        |
+ *    exchange     |               |               |
+ *                 |    Initialization complete    |
+ *                 |               |               |
+ */
+void dhcp6_start(void);
+
+#endif /* __DHCP6_H__ */

include/net.h
@@ -484,6 +484,8 @@ extern char	net_hostname[32];	/* Our hostname */
 #ifdef CONFIG_NET
 extern char	net_root_path[CONFIG_BOOTP_MAX_ROOT_PATH_LEN];	/* Our root path */
 #endif
+/* Indicates whether the pxe path prefix / config file was specified in dhcp option */
+extern char *pxelinux_configfile;
 /** END OF BOOTP EXTENTIONS **/
 extern u8		net_ethaddr[ARP_HLEN];		/* Our ethernet address */
 extern u8		net_server_ethaddr[ARP_HLEN];	/* Boot server enet address */
@@ -504,8 +506,8 @@ extern ushort		net_native_vlan;	/* Our Native VLAN */
 extern int		net_restart_wrap;	/* Tried all network devices */
 
 enum proto_t {
-	BOOTP, RARP, ARP, TFTPGET, DHCP, PING, PING6, DNS, NFS, CDP, NETCONS,
-	SNTP, TFTPSRV, TFTPPUT, LINKLOCAL, FASTBOOT, WOL, UDP, NCSI, WGET
+	BOOTP, RARP, ARP, TFTPGET, DHCP, DHCP6, PING, PING6, DNS, NFS, CDP,
+	NETCONS, SNTP, TFTPSRV, TFTPPUT, LINKLOCAL, FASTBOOT, WOL, UDP, NCSI, WGET
 };
 
 extern char	net_boot_file_name[1024];/* Boot File name */

net/Makefile
@@ -22,6 +22,7 @@ obj-$(CONFIG_IPV6)     += net6.o
 obj-$(CONFIG_CMD_NFS)  += nfs.o
 obj-$(CONFIG_CMD_PING) += ping.o
 obj-$(CONFIG_CMD_PING6) += ping6.o
+obj-$(CONFIG_CMD_DHCP6) += dhcpv6.o
 obj-$(CONFIG_CMD_PCAP) += pcap.o
 obj-$(CONFIG_CMD_RARP) += rarp.o
 obj-$(CONFIG_CMD_SNTP) += sntp.o

net/net.c
@@ -107,6 +107,8 @@
 #include <watchdog.h>
 #include <linux/compiler.h>
 #include <test/test.h>
+#include <net/tcp.h>
+#include <net/wget.h>
 #include "arp.h"
 #include "bootp.h"
 #include "cdp.h"
@@ -120,8 +122,7 @@
 #if defined(CONFIG_CMD_WOL)
 #include "wol.h"
 #endif
-#include <net/tcp.h>
-#include <net/wget.h>
+#include "dhcpv6.h"
 
 /** BOOTP EXTENTIONS **/
 
@@ -135,6 +136,8 @@ struct in_addr net_dns_server;
 /* Our 2nd DNS IP address */
 struct in_addr net_dns_server2;
 #endif
+/* Indicates whether the pxe path prefix / config file was specified in dhcp option */
+char *pxelinux_configfile;
 
 /** END OF BOOTP EXTENTIONS **/
 
@@ -510,6 +513,10 @@ restart:
 			dhcp_request();		/* Basically same as BOOTP */
 			break;
 #endif
+		case DHCP6:
+			if (IS_ENABLED(CONFIG_CMD_DHCP6))
+				dhcp6_start();
+			break;
 #if defined(CONFIG_CMD_BOOTP)
 		case BOOTP:
 			bootp_reset();

commit 806a22de450c945db76a1eb4c96aaa2c0e8b2a21

net: dhcp6: bound received DUID option lengths
dhcp6_parse_options() takes the SERVERID and CLIENTID option lengths
from the received ADVERTISE/REPLY and uses them unchecked:

- the SERVERID length is stored as server_uid_size and later copied into
  the fixed net_tx_packet buffer in dhcp6_send_request_packet() with no
  capacity check, so a large SERVERID overruns net_tx_packet (an
  out-of-bounds write);
- the CLIENTID length is passed straight to memcmp() against the fixed
  sm_params.duid buffer, so a length larger than it reads past the end
  (an out-of-bounds read).

Both are reachable by any on-link attacker that answers a DHCPv6 SOLICIT
during netboot; neither length is bounded against anything but the
received packet size.

Bound the SERVERID length to the RFC 8415 DUID maximum at parse time,
which also bounds the malloc() and the copy into the REQUEST for every
caller, and only compare a CLIENTID that is exactly the size of the
client DUID.

Fixes: a0245818f7f8 ("net: dhcp6: Add DHCPv6 (DHCP for IPv6)")
Signed-off-by: Shahriyar Jalayeri <shahriyar@byteray.co.uk>

net/dhcpv6.c
@@ -322,15 +322,19 @@ static void dhcp6_parse_options(uchar *rx_pkt, unsigned int len)
 
 		switch (ntohs(option_hdr->option_id)) {
 		case DHCP6_OPTION_CLIENTID:
-			if (memcmp(option_ptr, sm_params.duid, option_len)
-			    != 0) {
-				debug("CLIENT ID DOESN'T MATCH\n");
-			} else {
+			if (option_len == sizeof(sm_params.duid) &&
+			    !memcmp(option_ptr, sm_params.duid, option_len)) {
 				debug("CLIENT ID FOUND and MATCHES\n");
 				sm_params.rx_status.client_id_match = true;
+			} else {
+				debug("CLIENT ID DOESN'T MATCH\n");
 			}
 			break;
 		case DHCP6_OPTION_SERVERID:
+			if (option_len > DHCP6_DUID_MAX_LEN) {
+				debug("SERVER ID too long\n");
+				break;
+			}
 			sm_params.rx_status.server_id_found = true;
 			sm_params.rx_status.server_uid_ptr = (uchar *)option_hdr;
 			sm_params.rx_status.server_uid_size = option_len +

net/dhcpv6.h
@@ -37,6 +37,9 @@
 #define DUID_LL_SIZE		(sizeof(struct dhcp6_option_duid_ll) + ETH_ALEN)
 #define DUID_MAX_SIZE		DUID_LL_SIZE /* only supports DUID-LL currently */
 
+/* RFC 8415 sec 11.1: a DUID is a 2-octet type plus at most 128 octets */
+#define DHCP6_DUID_MAX_LEN	130
+
 /* vendor-class-data to send in vendor clas option */
 #define DHCP6_VCI_STRING	"U-Boot"
 